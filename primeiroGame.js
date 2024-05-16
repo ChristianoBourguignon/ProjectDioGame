fa5e0ee2cbf36dc04c673ae733153bf9ee48b9a4
Christiano Bourguignon <christianombl@gmail.com>
2024-05-15 23:32:14 -0300

    feat: otimização de código usando metódos e objetos

--- primeiroGame.js
+++ primeiroGame.js
@@ -38,10 +38,3 @@ const hero = {
 }
 
 console.log(hero.exp('Christiano', 50000));
-console.log(hero.exp('Matheus', 9500));
-console.log(hero.exp('Pedro', 8500));
-console.log(hero.exp('Patrick', 7500));
-console.log(hero.exp('Felipe Aguiar', 6500));
-console.log(hero.exp('Gabriel', 4500));
-console.log(hero.exp('Thiago', 1500));
-console.log(hero.exp('Julio', 900));
